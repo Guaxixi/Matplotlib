426a35764106033ee2dacd5060f355ce8952de2e
Xixi <roxie@live.cn>
2017-04-05 11:53:07 +0100

    comments

--- plot.py
+++ plot.py
@@ -9,11 +9,13 @@ x = []
 y = []
 
 with open('./test.csv','r') as csvfile:
-	lines = csv.reader(csvfile,delimiter=',')
+	lines = csv.reader(csvfile,delimiter=',')  
 	for row in lines:
 		x.append(float(row[0]))      ## here the data type should be float instead of int(integral), since it contains decimal potins.
 		y.append(float(row[1]))
 csvfile.close()     ### you need to close the file after you open it. 
+
+#### PS, you need to make sure there's no empty lines at the end of your data file, other wise the code get confused when searching for the data. and might show error message like : " IndexError: list index out of range"
 
 plt.plot(x,y,label='Cu foam')
 plt.xlabel('x')      ## xlabel could be more elegant than this
