82c3db49dd74938eff178bfd2b0b7ea00a497243
Xixi <roxie@live.cn>
2017-04-05 11:50:03 +0100

    comments

--- plot.py
+++ plot.py
@@ -11,16 +11,16 @@ y = []
 with open('./test.csv','r') as csvfile:
 	lines = csv.reader(csvfile,delimiter=',')
 	for row in lines:
-		x.append(float(row[0]))
+		x.append(float(row[0]))      ## here the data type should be float instead of int(integral), since it contains decimal potins.
 		y.append(float(row[1]))
-csvfile.close()
+csvfile.close()     ### you need to close the file after you open it. 
 
 plt.plot(x,y,label='Cu foam')
-plt.xlabel('x')
+plt.xlabel('x')      ## xlabel could be more elegant than this
 plt.ylabel('y')
-plt.title('E vs. I')
+plt.title('E vs. I')        
 plt.show()
-plt.savefig('./test.pdf')
+plt.savefig('./test.pdf')  ## also you need to save your figure, not just show it. 
 
 
 
